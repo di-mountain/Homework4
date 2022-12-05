0e93e2ed5dac40261f3563cf810118185d766db0
dimountain <gftght@rambler.ru>
2022-12-05 20:17:19 +0300

    Выполнена задача № 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+int age = 10;
+    System.out.println("Задача 1");
+if (age >= 18) {
+   System.out.println("Поздравляем тебя с совершеннолетием!");
+}
+if (age < 18) {
+    System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать...");
+}
     }
 }
